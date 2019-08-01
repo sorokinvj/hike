183600d56745ab66dbb8445490a0190c0eebb4c5
Vladislav Sorokin <sorokinvj@gmail.com>
2019-08-01 14:29:28 +0100

    remove getInitialProps  to tturn on automatic prerenderings

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -8,36 +8,36 @@ import MobileDetect from 'mobile-detect';
 // import GlobalStyle from '../components/globals';
 
 class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+  // static async getInitialProps({ Component, ctx }) {
+  //   let pageProps = {};
 
-    // добываем значение языка из пользовательских кукис
-    // const { language, useragreedwithcookies } = cookies(ctx);
-    // const hasCookies = typeof language !== 'undefined';
-    // const cookieConsent = typeof useragreedwithcookies !== 'undefined';
+  //   // добываем значение языка из пользовательских кукис
+  //   // const { language, useragreedwithcookies } = cookies(ctx);
+  //   // const hasCookies = typeof language !== 'undefined';
+  //   // const cookieConsent = typeof useragreedwithcookies !== 'undefined';
 
-    // определяем тип устройства, чтобы потом react-media
-    // рендерила именно ту версию компонента, которая
-    // совпадает с серверным html
-    const { req } = ctx;
-    const md = new MobileDetect(req ? req.headers['user-agent'] : '');
-    const phone = md.phone();
-    const tablet = md.tablet();
+  //   // определяем тип устройства, чтобы потом react-media
+  //   // рендерила именно ту версию компонента, которая
+  //   // совпадает с серверным html
+  //   const { req } = ctx;
+  //   const md = new MobileDetect(req ? req.headers['user-agent'] : '');
+  //   const phone = md.phone();
+  //   const tablet = md.tablet();
 
-    // Компонент получает свои pageProps с сервера
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-    // возвращаем pageProps с сервера и значение языка
-    return {
-      pageProps,
-      // hasCookies,
-      // language: hasCookies ? language : 'ru',
-      phone,
-      tablet,
-      // cookieConsent,
-    };
-  }
+  //   // Компонент получает свои pageProps с сервера
+  //   if (Component.getInitialProps) {
+  //     pageProps = await Component.getInitialProps(ctx);
+  //   }
+  //   // возвращаем pageProps с сервера и значение языка
+  //   return {
+  //     pageProps,
+  //     // hasCookies,
+  //     // language: hasCookies ? language : 'ru',
+  //     phone,
+  //     tablet,
+  //     // cookieConsent,
+  //   };
+  // }
 
   // componentDidMount() {
   //   // выставляем куки, если их не было
