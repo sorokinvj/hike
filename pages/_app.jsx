5ef68ef2bc87dcc171aa3b135706f96897a88d65
Vladislav Sorokin <sorokinvj@gmail.com>
2019-08-13 12:41:56 +0100

    all static

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,51 +5,51 @@ import Footer from '../components/footer'
 import GeneralHead from '../components/GeneralHead'
 import GlobalStyle from '../components/globals'
 import Navbar from '../components/navbar'
-import MobileDetect from 'mobile-detect';
+import MobileDetect from 'mobile-detect'
 
 // import cookies from 'next-cookies';
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-    const { req } = ctx;
-    const md = new MobileDetect(req ? req.headers['user-agent'] : '');
-    const phone = md.phone();
-    const tablet = md.tablet();
+    let pageProps = {}
+    const { req } = ctx
+    const md = new MobileDetect(req ? req.headers['user-agent'] : '')
+    const phone = md.phone()
+    const tablet = md.tablet()
 
     // Компонент получает свои pageProps с сервера
     if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
+      pageProps = await Component.getInitialProps(ctx)
     }
     // возвращаем pageProps с сервера и значение языка
     return {
       pageProps,
       phone,
       tablet,
-    };
+    }
   }
 
   render() {
     const {
       Component, pageProps, phone, tablet, cookieConsent,
-    } = this.props;
+    } = this.props
 
     const theme = {
       '$grid-gutter-width': '30px',
       "$darkblue": '#002B5D',
-    };
+    }
     
     return (
       <Container>
         <BootstrapProvider reset theme={theme}>
-            <GlobalStyle />
-            <Navbar />
-            <GeneralHead />
-            <Component {...pageProps} phone={phone} tablet={tablet} />
-            <Footer />
+          <GlobalStyle />
+          <Navbar />
+          <GeneralHead />
+          <Component {...pageProps} phone={phone} tablet={tablet} />
+          <Footer />
         </BootstrapProvider>
       </Container>
-    );
+    )
   }
 }
 
-export default MyApp;
+export default MyApp
