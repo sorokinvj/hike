5f4197fcf680e9e455a5525e012f8fea5eb245b7
Vladislav Sorokin <sorokinvj@gmail.com>
2019-09-11 23:47:07 +0100

    h2 - regular

--- components/style/H2.js
+++ components/style/H2.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 const H2 = styled.h2`
     font-family: 'Nunito Sans', sans-serif;
     font-style: normal;
-    font-weight: 900;
     font-size: 3.8rem;
     line-height: 4rem;
     @media (min-width: 416px) {
